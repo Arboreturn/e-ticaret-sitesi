e393502ca04114731c2ed2a3c596af0453f0f5ad
Efecan <efecanozkoparan05@gmail.com>
2022-10-22 00:39:17 +0300

    "lesson2"

--- kaft_clone/settings.py
+++ kaft_clone/settings.py
@@ -126,7 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+#    '/var/www/static/', daha fazla static file eklemek için kullanılır
+]
 
 # Apss:
 # Page
