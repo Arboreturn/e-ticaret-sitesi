44e74b92d8102c83ff9cbbdbb1809513e894b749
Efecan <efecanozkoparan05@gmail.com>
2022-11-16 01:57:29 +0300

    "added page and manage"

--- page/urls.py
+++ page/urls.py
@@ -1,14 +1,27 @@
 from django.urls import path
 from .views import (
+    # Manage:
+    manage_list,
+    
+    # Page:
+    page_list,
+    
+    # Carousel:
     carousel_create,
     carousel_list,
-    carousel_update,                   
+    carousel_update,              
 )
 
 
 urlpatterns = [
+    path('',manage_list,name="manage_list"),
+    
+    # Carousel: 
     path('carousel_list/',carousel_list,name="carousel_list"),
     path('carousel_create/',carousel_create,name="carousel_create"),
     path('carousel_update/<int:pk>/',carousel_update,name="carousel_update"),
+    
+    # Page:
+    path('page/list/',page_list,name="page_list"),
 ]
 
--- page/views.py
+++ page/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import Carousel
+from .models import Carousel,Page
 from .forms import CarouselFormModel
 # Create your views here.
 
@@ -10,8 +10,21 @@ def index(request):
     context['images'] = Carousel.objects.filter(
         status="published"
         # we must saying there is cover image
-        ).exclude(cover_image='').count() # we filtered the unofficial ones
+        ) # we filtered the unofficial ones
     return render(request,'home/index.html', context)
+
+
+#
+def manage_list(request):
+    context=dict()
+    return render(request,'manage/manage.html',context)
+
+
+def page_list(request):
+    context= dict()
+    context['items']=Page.objects.all().order_by('-pk')
+    return render(request,'manage/page_list.html',context)
+
 
 # Admin:
 def carousel_list(request):
@@ -20,28 +33,37 @@ def carousel_list(request):
     return render(request,'manage/carousel_list.html',context)
 
 
-def carousel_update(request,pk):
-    context=dict()
+# stuff not checked
+def carousel_update(request, pk): # abi bana pk vermessen gelmem :)
+    context = dict()
     # kaft_clone.com/manage/carousel/1/edit
+    # Show :
     item = Carousel.objects.get(pk=pk)
-    context['item']= Carousel.objects.get(pk=pk)
-    return render(request,'manage/carousel_form.html',context)
-    
+    context['title'] = f"{item.title } - pk:{item.pk} Carousel Create Form"
+    context['form'] = CarouselFormModel(instance=item)
+    if request.method == 'POST':
+        form = CarouselFormModel(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            # return redirect('carousel_list')
+            messages.success(request, 'guncellendi ;)')
+            return redirect('carousel_update', pk)
+    return render(request, 'manage/carousel_form.html', context)
+
+
 
 # stuff not checked
 def carousel_create(request):
-    context=dict()
-    context ['form'] = CarouselFormModel()
-    # item = Carousel.objects.first() --> current id
-    # context ['form'] = CarouselFormModel(instance=item)
-    if request_method== 'POST':
-        print(request.POST)
-        print(request.FILES.get['cover_image'])
-        # create code is deleted
-        form = CarouselFormModel(request.POST,request.FILES)
+    context = dict()
+    context['title'] = "Carousel Create Form"
+    context['form'] = CarouselFormModel()
+    
+    if request.method == 'POST':
+        print(request.FILES.get('cover_image'))
+        form = CarouselFormModel()(request.POST, request.FILES)
         print(form)
-        if form.is_valid(): 
+        if form.is_valid():
             form.save()
-        messages.success(request,'Birseyler eklendi ama bilemiyorum')
-        return render(request,'manage/carousel_form.html',context)
+        messages.success(request, 'Birseyler eklendi ama ne oldu bilemiyorum')
+    return render(request, 'manage/carousel_form.html', context)
         
