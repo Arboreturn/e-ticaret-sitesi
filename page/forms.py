bbbfa02f9df857208cb60b2be6d5553d15566967
Efecan <efecanozkoparan05@gmail.com>
2022-11-16 21:44:55 +0300

    "edited page"

--- page/forms.py
+++ page/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from .models import Carousel
+from .models import Carousel, Page
 
 # Create the form class.
 class CarouselFormModel(ModelForm): # forms come from model form
@@ -9,6 +9,19 @@ class CarouselFormModel(ModelForm): # forms come from model form
         fields = [
             'title',
             'cover_image',
+            'status',
         ]
     
-    
+
+class PageModelForm(ModelForm):
+    class Meta:
+        model=Page
+        # fields = '__all__' # Don't use this
+        # exclude = ['title'] # get everything except the title
+        fields =[
+            'title',
+            'cover_image',
+            'content',
+            'status',
+        ]
+        
--- page/views.py
+++ page/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.utils.text import slugify
 from .models import Carousel,Page
-from .forms import CarouselFormModel
+from .forms import CarouselFormModel,PageModelForm
+from django.contrib.admin.views.decorators import staff_member_required
 # Create your views here.
 
 # User:
@@ -19,6 +21,53 @@ def manage_list(request):
     context=dict()
     return render(request,'manage/manage.html',context)
 
+
+@staff_member_required
+def page_list(request):
+    context = dict()
+    context['items'] = Page.objects.all().order_by('-pk')
+    return render(request, 'manage/page_list.html', context)
+
+
+def page_create(request):
+    context = dict()
+    context["title"]="Page Create Form"
+    context['form'] = PageModelForm()
+    # from django.utils.text import slugify
+    if request.method == 'POST':
+        print(request.FILES.get('cover_image'))
+        form = PageModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            item=form.save(commit=False)
+            item.slug= slugify(item.title.replace('ı','i'))
+            item.save()
+        messages.success(request, 'Birseyler eklendi ')
+    return render(request, 'manage/form.html', context)
+        
+
+def page_update(request, pk): # abi bana pk vermessen gelmem :)
+    context = dict()
+    item = Page.objects.get(pk=pk)
+    context['title'] = f"{item.title } - pk:{item.pk} Page Update Form"
+    context['form'] = PageModelForm(instance=item)
+    if request.method == 'POST':
+        form = PageModelForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            item = form.save(commit=False)
+            if item.slug =="":
+                item.slug= slugify(item.title.replace('ı','i'))
+            item.save()
+            # return redirect('carousel_list')
+            messages.success(request, 'guncellendi ;)')
+            return redirect('page_update', pk)
+    return render(request, 'manage/form.html', context)
+
+
+def page_delete(request,pk):
+    item=Page.objects.get(pk=pk)
+    item.status="deleted"
+    item.save()
+    return redirect('page_list')
 
 def page_list(request):
     context= dict()
@@ -48,7 +97,7 @@ def carousel_update(request, pk): # abi bana pk vermessen gelmem :)
             # return redirect('carousel_list')
             messages.success(request, 'guncellendi ;)')
             return redirect('carousel_update', pk)
-    return render(request, 'manage/carousel_form.html', context)
+    return render(request, 'manage/form.html', context)
 
 
 
@@ -60,10 +109,10 @@ def carousel_create(request):
     
     if request.method == 'POST':
         print(request.FILES.get('cover_image'))
-        form = CarouselFormModel()(request.POST, request.FILES)
+        form = CarouselFormModel(request.POST, request.FILES)
         print(form)
         if form.is_valid():
             form.save()
         messages.success(request, 'Birseyler eklendi ama ne oldu bilemiyorum')
-    return render(request, 'manage/carousel_form.html', context)
+    return render(request, 'manage/form.html', context)
         
