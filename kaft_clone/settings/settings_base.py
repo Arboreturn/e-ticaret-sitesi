9167ac46f3d7a80df7e758bc559e4e3b09d510f8
Efecan <efecanozkoparan05@gmail.com>
2022-11-22 23:29:21 +0300

    project finished

--- kaft_clone/settings/settings_base.py
+++ kaft_clone/settings/settings_base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'kaft_clone.middleware.session_check_middleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
